1330f472a70c3d7eded0097aacd5aa5e8af2a3bb
codewithashim <codewithashim@gmail.com>
2022-12-30 19:06:13 +0600

    update data

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -26,6 +26,13 @@ const Home = () => {
     const { data, loading, error } = useFatch('https://jsonplaceholder.typicode.com/users')
     console.log(data)
 
+    const [search, setSearch] = React.useState('')
+
+    const handleSearch = (e: any) => {
+        setSearch(e.target.value)
+
+    }
+
     return (
         <>
             {
@@ -35,13 +42,16 @@ const Home = () => {
             }
 
             <div>
-                
+                <form >
+                    <input type="text" placeholder="Type here" className="input input-bordered input-secondary w-full" />
+                    <button className="btn btn-primary mx-2" onClick={handleSearch}>Search</button>
+                </form>
             </div>
 
 
             <section>
                 <Container>
-                    <div className=''>
+                    <div className='felx gap-4'>
                         {
                             (loading === false && error === false) && data.map((user: any) => {
                                 console.log(user)
